324e0a4443a589a899eea88f8f7511227dba9379
Arvind Ravulavaru <arvind.ravulavaru@gmail.com>
2015-02-17 14:59:18 +0530

    CORS

--- server/app.js
+++ server/app.js
@@ -1,43 +1,48 @@
 (function() {
-  'use strict';
+    'use strict';
 
-  var express = require('express');
-  var path = require('path');
-  var logger = require('morgan');
-  var cookieParser = require('cookie-parser');
-  var bodyParser = require('body-parser');
+    var express = require('express');
+    var path = require('path');
+    var logger = require('morgan');
+    var cookieParser = require('cookie-parser');
+    var bodyParser = require('body-parser');
 
-  var routes = require('./routes/index');
+    var routes = require('./routes/index');
 
-  var app = express();
+    var app = express();
 
-  // view engine setup
-  app.set('views', path.join(__dirname, 'views'));
-  app.engine('html', require('ejs').renderFile);
-  app.set('view engine', 'html');
+    // view engine setup
+    app.set('views', path.join(__dirname, 'views'));
+    app.engine('html', require('ejs').renderFile);
+    app.set('view engine', 'html');
 
-  app.use(logger('dev'));
-  app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({
-    extended: true
-  }));
-  app.use(cookieParser());
+    app.use(logger('dev'));
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({
+        extended: true
+    }));
+    app.use(cookieParser());
 
-  app.use(express.static(path.join(__dirname, '../client')));
+    app.use(express.static(path.join(__dirname, '../client')));
 
-  app.all('*', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With,X-Powered-By,Content-Type");
-    next();
-  });
+    app.all('*', function(req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+        res.header("Access-Control-Allow-Headers", "X-Requested-With,X-Powered-By,Content-Type");
+        if (req.method === 'OPTIONS') {
+            res.status(200).end();
+        } else {
+            next();
+        }
+    });
 
-  app.use('/', routes);
+    app.use('/', routes);
 
-  app.set('port', process.env.PORT || 3000);
+    app.set('port', process.env.PORT || 3000);
 
-  var server = app.listen(app.get('port'), function() {
-    console.log('Express server listening on port ' + server.address().port);
-  });
+    var server = app.listen(app.get('port'), function() {
+        console.log('Express server listening on port ' + server.address().port);
+    });
 
-  module.exports = app;
+    module.exports = app;
 }());
