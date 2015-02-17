1835d058be9ab1679f541c2a3061f74fdd35035e
Arvind Ravulavaru <arvind.ravulavaru@gmail.com>
2015-02-17 14:49:36 +0530

    CORS

--- server/app.js
+++ server/app.js
@@ -27,7 +27,7 @@
 
   app.all('*', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Headers", "Origin,X-Requested-With,X-Powered-By,Content-Type,Accept");
     next();
   });
 
